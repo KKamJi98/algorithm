497115f9b378f2e3ca121672f467b00c8e80066c
Oxi <xowl5460@naver.com>
2023-06-28 18:40:05 +0900

    OddEvenChecker

--- Algorithm_Java/OddEvenChecker/src/Main.java
+++ Algorithm_Java/OddEvenChecker/src/Main.java
@@ -1,17 +1,19 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		// Press Alt+Enter with your caret at the highlighted text to see how
-		// IntelliJ IDEA suggests fixing it.
-		System.out.printf("Hello and welcome!");
-
-		// Press Shift+F10 or click the green arrow button in the gutter to run the code.
-		for (int i = 1; i <= 5; i++) {
-
-			// Press Shift+F9 to start debugging your code. We have set one breakpoint
-			// for you, but you can always add more by pressing Ctrl+F8.
-			System.out.println("i = " + i);
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.print("판별할 정수를 입력해 주세요(종료 = 0 입력) => ");
+			long numInput = sc.nextLong();
+			if(numInput == 0){
+				break;
+			}
+			if (numInput % 2 == 0) {
+				System.out.printf("숫자 %d는 짝수입니다.\n", numInput);
+			} else {
+				System.out.printf("숫자 %d는 홀수입니다.\n", numInput);
+			}
 		}
 	}
 }
