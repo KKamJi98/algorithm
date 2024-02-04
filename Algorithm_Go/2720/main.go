c15a4693d198dd7115ecff6033abf177e4fa4aec
KKam._.Ji <xowl5460@naver.com>
2024-02-04 19:49:23 +0900

    you're doing great

---
+++ Algorithm_Go/2720/main.go
@@ -0,0 +1,40 @@
+// https://www.acmicpc.net/problem/2720 [세탁소 사장 동혁]
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var T int
+	fmt.Fscan(reader, &T)
+
+	for i := 0; i < T; i++ {
+		var C int
+		fmt.Fscan(reader, &C)
+		getExchange(C, writer)
+	}
+}
+
+func getExchange(C int, writer *bufio.Writer) {
+	arr := []int{
+		25, 10, 5, 1,
+	}
+
+	for C > 0 {
+		if C  == 0 {
+			break
+		}
+		for _, coin := range arr {
+			fmt.Fprintf(writer, "%d ", C / coin)
+			C = C % coin
+		}
+	}
+	fmt.Fprintln(writer, "")
+}
