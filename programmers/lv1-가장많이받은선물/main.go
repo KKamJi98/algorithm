e08e43651516c1f34fee5fbc0fb52e60fc2e11d2
kkankkandev <xowl5460@naver.com>
2024-02-06 20:53:55 +0900

    you're doing great

--- programmers/lv1-가장많이받은선물/main.go
+++ programmers/lv1-가장많이받은선물/main.go
@@ -15,16 +15,43 @@ func main() {
 }
 
 func solution(friends []string, gifts []string) int {
-	friendMap := make(map[string]int)
+	friendMapScore := make(map[string]int)
+	friendMapTo := make(map[string][string]int)
 
 	for _, friend := range friends {
 		friendMap[friend] = 0
+		for _, To := range friends {
+			friendMapTo[friend][To] = 0
+		}
 	}
 
 	for _, gift := range gifts {
 		giftSlice := strings.Split(" ", gift)
+		friendMapTo[giftSlice[0][giftSlice[1]]]++
 		friendMap[giftSlice[0]]++
 		friendMap[giftSlice[1]]--
+	}
+
+	for from, to := range friendMapTo {
+		for _, count := range to {
+			if friendMapTo[from][to] == friend[to][from] {
+				if friendMapScore[from] < friendMapScore[to] {
+					friendMapScore[to]++
+				} else if friendMapScore[from] > friendMapScore[to] {
+					friendMapScore[from]++
+				} else {
+					continue
+				}
+			} else {
+				if friendMapTo[from][to] < friendMapTo[to][from] {
+					friendMapScore[from]++
+				} else if friendMapTo[from][to] > friendMapTo[to][from] {
+					friendMapScore[to]++
+				} else {
+					continue
+				}
+			}
+		}
 	}
 
 	for key, gift range friendMap {
