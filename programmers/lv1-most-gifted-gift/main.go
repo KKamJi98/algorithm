b9494a1a18ab1e02a680ada4d2c4d4c0f5a2bc1a
KKam._.Ji <xowl5460@naver.com>
2024-02-07 01:41:54 +0900

    you're doing great

--- programmers/lv1-most-gifted-gift/main.go
+++ programmers/lv1-most-gifted-gift/main.go
@@ -12,7 +12,6 @@ func solution(friends []string, gifts []string) int {
 	getCount := make(map[string]int)
 
 	for _, friend := range friends {
-		friendMapScore[friend] = 0
 		friendMapTo[friend] = make(map[string]int)
 		getCount[friend] = 0
 	}
@@ -34,22 +33,20 @@ func solution(friends []string, gifts []string) int {
 				if friendMapScore[from] > friendMapScore[to] {
 					getCount[from]++
 				}
-				} else { //두 사람이 선물을 주고받은 기록이 있다면, 이번 달까지 두 사람 사이에 더 많은 선물을 준 사람이 다음 달에 선물을 하나 받습니다.
-					if friendMapTo[from][to] > friendMapTo[to][from] {
-							getCount[from]++
-						} 
-					}
-				}
+			} else { //두 사람이 선물을 주고받은 기록이 있다면, 이번 달까지 두 사람 사이에 더 많은 선물을 준 사람이 다음 달에 선물을 하나 받습니다.
+				if friendMapTo[from][to] > friendMapTo[to][from] {
+						getCount[from]++
+				} 
 			}
-			
-			var maxCount int = 0
-			for key, count := range getCount {
-				fmt.Printf("%s : %d\n", key, count)
-				if count > maxCount {
-					maxCount = count
-				}
-			}
-
+		}
+	}	
+	var maxCount int = 0
+	for key, count := range getCount {
+		fmt.Printf("%s : %d\n", key, count)
+		if count > maxCount {
+			maxCount = count
+		}
+	}
 	return maxCount
 }
 
