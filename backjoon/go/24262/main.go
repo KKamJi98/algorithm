fd51094623e4343d305532120356bc2d71289b53
War-Oxi <xowl5460@naver.com>
2024-02-09 22:03:42 +0900

    you're doing great

---
+++ backjoon/go/24262/main.go
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/24262 - [알고리즘 수업 - 알고리즘의 수행 시간 1]
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+	var n int
+    fmt.Scan(&n)
+	
+	fmt.Println(1)
+	fmt.Println(0)
+}
---
+++ backjoon/go/24263/main.go
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/submit/24263 - [ 알고리즘 수업 - 알고리즘의 수행 시간 2 ]
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var n int 
+	fmt.Scan(&n)
+	fmt.Printf("%d\n%d", n, 1)
+}
