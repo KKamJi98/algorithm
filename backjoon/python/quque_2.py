69831ce0302b096203c6138d82c2cb57a417a491
kkankkandev <xowl5460@naver.com>
2024-02-20 14:52:36 +0900

    you're doing great

---
+++ backjoon/python/quque_2.py
@@ -0,0 +1,58 @@
+# https://www.acmicpc.net/problem/18258 - [ 큐 2 ]
+import sys
+
+class KKamQueue: 
+    def __init__(self):
+        self.queue = [0] * 2000001
+        self.start: int = 0
+        self.end: int = 0
+    
+    def push(self, num:int):
+        self.queue[self.end] = num
+        self.end += 1
+    def pop(self) -> int:
+        if self.start == self.end:
+            return -1
+        else:
+            self.start += 1
+            return self.queue[self.start-1]
+    def size(self) -> int:
+        return self.end - self.start
+    def empty(self) -> bool:
+        if self.end - self.start == 0:
+            return True
+        else:
+            return False
+    def front(self) -> int:
+        if self.empty():
+            return -1
+        else:
+            return self.queue[self.start]
+    def back(self) -> int:
+        if self.empty():
+            return -1
+        else:
+            return self.queue[self.end-1]
+
+def solution(queueIn):
+    N = int(sys.stdin.readline())
+    
+    for i in range(N):
+        command = sys.stdin.readline().rstrip().split(" ")
+        if command[0] == "push":
+            # print(int(command[1]))
+            queueIn.push(int(command[1]))
+        elif command[0] == "pop":
+            print(queueIn.pop())
+        elif command[0] == "size":
+            print(queueIn.size())
+        elif command[0] == "empty":
+            print(int(queueIn.empty()))
+        elif command[0] == "front":
+            print(queueIn.front())
+        elif command[0] == "back":
+            print(queueIn.back())
+    
+
+myQueue = KKamQueue()
+solution(myQueue)
---
+++ programmers/python/lv1/walking_park.py
@@ -0,0 +1,73 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/172928 - [ 공원 산책 ]
+
+def solution(park, routes):
+    hIdx = 0
+    wIdx = 0
+    for H in range(len(park)):
+        for W in range(len(park[H])):
+            if park[H][W] == 'S':
+                hIdx = H
+                wIdx = W
+                break
+    for route in routes:
+        checker = True
+        tmpH = hIdx
+        tmpW = wIdx 
+        command = route.split(" ")
+        command[1] = int(command[1])
+        if command[0] == 'E':
+            for _ in range (command[1]):
+                if tmpW + 1 >= len(park[tmpH]):
+                    checker = False
+                    break
+                tmpW += 1
+                if park[tmpH][tmpW] == 'X':
+                    checker = False
+                    break
+            if checker:
+                wIdx = tmpW
+        elif command[0] == 'W':
+            for _ in range (command[1]):
+                if tmpW - 1 < 0:
+                    checker = False
+                    break; 
+                tmpW -= 1
+                if park[tmpH][tmpW] == 'X':
+                    checker = False
+                    break
+            if checker:
+                wIdx = tmpW
+        elif command[0] == 'N':
+            for _ in range (command[1]):
+                if tmpH - 1 < 0:
+                    checker = False
+                    break
+                tmpH -= 1
+                if park[tmpH][tmpW] == 'X':
+                    checker = False
+                    break
+            if checker:
+                hIdx = tmpH
+        elif command[0] == 'S':
+            for _ in range (command[1]):
+                if tmpH + 1 >= len(park[tmpH]):
+                    checker = False
+                    break
+                tmpH += 1
+                if park[tmpH][tmpW] == 'X':
+                    checker = False
+                    break
+            if checker:
+                hIdx = tmpH
+        else:
+            print("Wrong Input")
+    return hIdx, wIdx
+            
+                
+
+
+park = ["SOO","OOO","OOO"]
+routes = ["E 2","S 2","W 1"]	
+print(solution(park, routes))
+# solution(park, routes)
+# print(len(park[0]))
