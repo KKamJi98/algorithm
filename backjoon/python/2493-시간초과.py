6592383c6a6d3d7bc69e662b103dd8d0606cfdb4
kkankkandev <xowl5460@naver.com>
2024-02-21 14:26:27 +0900

    you're doing great

---
+++ backjoon/python/2493-시간초과.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/problem/2493 - [ 탑 ]
+# 시간 초과 발생 => Stack의 기능 출력하는 것에서만 사용함 Stack을 사용해 O(n^2)에서  O(n)으로 시간 복잡도 개선 가능
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+kkamList = list(map(int, list(sys.stdin.readline().rstrip().split(" "))))
+stack = []
+
+for i in reversed(range(N)):
+    for j in reversed(range(i)):
+        curHeight = kkamList[i]
+        if kkamList[j] >= curHeight:
+            stack.append(j+1)
+            break
+        if j == 0:
+            stack.append(0)
+            
+stack.append(0)
+
+while len(stack) > 0:
+    print(stack.pop(), end=" ")
---
+++ programmers/python/lv1/food_fight.py
@@ -0,0 +1,14 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/134240 - [ 푸드 파이트 대회 ]
+
+def solution(food):
+    answer = str()
+    for i in range(1, len(food)):
+        quantity = food[i] // 2
+        for j in range(quantity):
+            answer += str(i)
+            
+    reverseAnswer = reversed(answer)
+    return answer+"0"+answer[::-1]
+
+food = [1, 3, 4, 6]	
+print(solution(food))
