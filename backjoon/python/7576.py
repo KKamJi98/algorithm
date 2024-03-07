73ed1b0277455490152f07f27e56c9caf98820ff
KKam._.Ji <xowl5460@naver.com>
2024-03-07 23:32:26 +0900

    you're doing great

--- backjoon/python/7576.py
+++ backjoon/python/7576.py
@@ -6,19 +6,17 @@ col, row = map(int, sys.stdin.readline().rstrip().split())
 
 tomatos = []
 dist = [[-1 for _ in range(col)] for _ in range(row)]
-# print(dist)
+
 for _ in range(row):
     tomatos.append(list(map(int, sys.stdin.readline().rstrip().split())))
-    
 Q = deque()
 for r in range(len(tomatos)):
     for c in range(len(tomatos[r])):
         if tomatos[r][c] == 1:
             Q.append((r, c))
             dist[r][c] = 0
-            
-            
 days = 0
+
 while Q:
     mv = [[1,0], [-1,0], [0,-1], [0,1]]
     curRow, curCol = Q.popleft()
@@ -32,7 +30,7 @@ while Q:
         dist[nextRow][nextCol] = dist[curRow][curCol] + 1
         if dist[nextRow][nextCol] > days:
             days = dist[nextRow][nextCol]
-            
+
 for r in tomatos:
     for c in r:
         if c == 0:
