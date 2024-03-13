fa864d4467df26653095bc11c900a01e39615d95
kkankkandev <xowl5460@naver.com>
2024-03-13 10:37:27 +0900

    CREATE: TCP, UDP Post Update

--- backjoon/python/test.py
+++ backjoon/python/test.py
@@ -1,8 +1,18 @@
-from collections import deque
-deque = deque()
-print(len(deque))
-deque.append(5)
-print(len(deque))
+# https://www.acmicpc.net/problem/2579 - [계단 오르기]
+import sys
 
-def add(a, b):
-    return a + b
+input = sys.stdin.readline
+
+n = int(input())
+stairs = [int(input()) for _ in range(n)]
+dp = [0] * n
+
+if n == 1:
+    print(stairs[0])
+else:
+    dp[0] = stairs[0]
+    dp[1] = stairs[0] + stairs[1]
+    dp[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2])
+    for i in range(3, n):
+        dp[i] = max(dp[i - 3] + stairs[i - 1] + stairs[i], dp[i - 2] + stairs[i])
+    print(dp[n - 1])
