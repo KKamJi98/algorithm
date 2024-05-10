8c72d0886512e93144a6c166c499e8a728af02c4
War-Oxi <xowl5460@naver.com>
2024-05-10 20:23:03 +0900

    Linked Linke (not complete)

--- Data_Structure/linked_list.py
+++ Data_Structure/linked_list.py
@@ -18,13 +18,40 @@ class LinkedList(object):
                 current = current.next
             current.next = new_node
 
+    def get(self, idx):
+        cur = self.head
+        cur_idx = 0
+        while cur_idx != idx:
+            if cur.next == None:
+                return None
+            cur_idx += 1
+            cur = cur.next
+        return cur.data
+        
+    def insert_at(self, idx, value):
+        pass
+    
+    def remove_at(self, idx):
+        pass
+    
+    def insert_back(self, value): # O(1) tail need
+        pass
+
+    
+
 ll = LinkedList()
 ll.append(1)
 ll.append(2)
 ll.append(3)
 ll.append(4)
 
-cur = ll.head
-while cur.next:
-    print(cur.data)
-    cur = cur.next
+print(ll.get(0))
+print(ll.get(1))
+print(ll.get(3))
+
+# print(ll.__sizeof__)
+
+# cur = ll.head
+# while cur.next:
+#     print(cur.data)
+#     cur = cur.next
