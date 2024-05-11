e14ed7faa2bd090c82202a02811a804ae41a11d5
War-Oxi <xowl5460@naver.com>
2024-05-11 18:59:48 +0900

    you're doing great

--- Data_Structure/linked_list.py
+++ Data_Structure/linked_list.py
@@ -1,57 +1,103 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+  def __init__(self, data):
+    self.data = data
+    self.next = None
 
 
 class LinkedList(object):
-    def __init__(self):
-        self.head = None
+  def __init__(self):
+    self.head = None
+    self.tail = None
 
-    def append(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-        else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = new_node
+  def append(self, value):
+    new_node = Node(value)
+    if self.head is None:
+      self.head = new_node
+    else:
+      current = self.head
+      while current.next:
+        current = current.next
+      current.next = new_node
+    self.tail = new_node
 
-    def get(self, idx):
-        cur = self.head
-        cur_idx = 0
-        while cur_idx != idx:
-            if cur.next == None:
-                return None
-            cur_idx += 1
-            cur = cur.next
-        return cur.data
-        
-    def insert_at(self, idx, value):
-        pass
-    
-    def remove_at(self, idx):
-        pass
-    
-    def insert_back(self, value): # O(1) tail need
-        pass
+  def get(self, idx):
+    cur = self.head
+    cur_idx = 0
+    while cur_idx != idx:
+      if cur.next == None:
+        return None
+      cur_idx += 1
+      cur = cur.next
+    return cur.data
 
-    
+  def get_node(self, idx):
+    cur = self.head
+    cur_idx = 0
+    while cur_idx != idx:
+      if cur.next == None:
+        print("index out of range")
+        return
+      cur_idx += 1
+      cur = cur.next
+    return cur
+
+  def insert_at(self, idx, value):
+    if idx == 0:
+      tmp_node = self.head
+      self.head = Node(value)
+      self.head.next = tmp_node
+      return
+    else:
+      cur_idx = 0
+      cur = self.head
+      while cur_idx != idx - 1:
+        if cur.next == None:
+          print("index out of range")
+          return
+        cur_idx += 1
+        cur = cur.next
+      new_node = Node(value)
+      new_node.next = cur.next
+      cur.next = new_node
+
+  def remove_at(self, idx):
+    if idx == 0:
+      self.head = self.head.next
+    else:
+      cur_node = self.get_node(idx-1)
+      if cur_node.next == None:
+        print("index out of range")
+        return
+      cur_node.next = cur_node.next.next
+
+  def insert_back(self, value):  # O(1) tail need
+    if self.head is None:
+      self.head = Node(value)
+      self.tail = self.head
+    else:
+      self.tail.next = Node(value)
+      self.tail = self.tail.next
+
 
 ll = LinkedList()
+ll.insert_back(9)
 ll.append(1)
 ll.append(2)
 ll.append(3)
 ll.append(4)
+ll.insert_at(0, 0)
+ll.remove_at(5)
+ll.insert_back(8)
+ll.insert_back(7)
 
-print(ll.get(0))
-print(ll.get(1))
-print(ll.get(3))
+# 출력
+cur = ll.head
+while True:
+  print(cur.data)
+  if not cur.next:
+    break
+  cur = cur.next
 
-# print(ll.__sizeof__)
-
-# cur = ll.head
-# while cur.next:
-#     print(cur.data)
-#     cur = cur.next
+idx = 0
+if ll.get_node(idx):
+  print(f"idx => {idx} \t data => {ll.get_node(idx).data}")
--- backjoon/python/15683.py
+++ backjoon/python/15683.py
@@ -5,33 +5,57 @@ import sys
 input = sys.stdin.readline
 
 # 12 -> 3 -> 6 -> 9
-cctv_watch = {
+cctv_type = {
     1: [[0], [1], [2], [3]],
     2: [[0, 2], [1, 3]],
     3: [[0, 1], [1, 2], [2, 3], [0, 3]],
     4: [[0, 1, 2], [1, 2, 3], [2, 3, 0], [3, 0, 1]],
     5: [[0, 1, 2, 3],]
 }
-
+# print(cctv_type[1])
 mv = [[0, 1], [1, 0], [0, -1], [-1, 0]]  # [x, y] 12 -> 3 -> 6 -> 9
 board = []
 min_area = 0
 
 
 def check(x_pos, y_pos):
-    return x_pos < 0 or y_pos >= N or x_pos >= M or y_pos < 0
+    # print(f"M => {M}\tN => {N}")
+    return x_pos >=0 and y_pos >= 0 and x_pos < M and y_pos < N and board[y_pos][x_pos] != 6
 
+def fill(x_pos, y_pos, direct, board_in):
+    next_x = x_pos + mv[direct][0]
+    next_y = y_pos + mv[direct][1]
+    while check(next_x, next_y):
+        board_in[next_y][next_x] = -1
+        next_x += mv[direct][0]
+        next_y += mv[direct][1]
+    
+#TODO
+def watch(cctv_list, board):  # 일자감시 cctv_list => cctv_type, x_pos, y_pos 
+    pass
+    # 첫번째 꺼 cur = cctv_list.pop() 방향 => for i in range(cctv_type[cur[2]])
+        # 채우기 => 함수로 분리 fill(direct)함수 for i in range(cctv_type)
+        # 다음꺼 확인 cctv_list.pop() 방향 => cur = for i in range(cctv_type[cur[2]])
+        # ... 반복 until cctv_list is empty
+    
 
-def watch(cctv_info, board):  # 일자감시 cctv_info => cctv_type, x_pos, y_pos
 
 
 if __name__ == "__main__":
     N, M = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(N)]
-    cctv_info = list()
+    cctv_list = list()
     for i in range(N):
         for j in range(M):
             if 1 <= board[i][j] and board[i][j] <= 5:
-                cctv_info.append(j, i, board[i][j])  # x_pos, y_pos, cctv_type
+                cctv_list.append([j, i, board[i][j]])  # x_pos, y_pos, cctv_type
             elif board[i][j] == 0:
                 min_area += 1
+                
+    # test
+    fill(2, 2, 2, board)
+    print()
+    for i in range (N):
+        print(*board[i])
+    print()
+    print(cctv_list)
