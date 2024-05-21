7ef5ae3847859baef6c89a0eb5ef1516881dbe0b
War-Oxi <xowl5460@naver.com>
2024-05-21 13:32:03 +0900

    you're doing great

---
+++ leetcode/Easy/1-two-sum.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/two-sum/
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        new_dict = {}
+        for idx, num in enumerate(nums):
+            if target - num in new_dict and idx != new_dict[target - num]:
+                return [new_dict[target - num], idx]
+            new_dict[num] = idx
+
+
+S = Solution()
+print(S.twoSum([3,3], 6))
