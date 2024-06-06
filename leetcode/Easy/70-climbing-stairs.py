9defb740b0ae733eb948e79f1126bbb6f98442f2
War-Oxi <xowl5460@naver.com>
2024-06-06 15:49:45 +0900

    you're doing great

--- leetcode/Easy/70-climbing-stairs.py
+++ leetcode/Easy/70-climbing-stairs.py
@@ -30,6 +30,12 @@ class Solution:
             dp[i] = dp[i - 1] + dp[i - 2]
         return dp[n]
 
+    def bruteForce(self, n: int) -> int:
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        return self.bruteForce(n-1) + self.bruteForce(n-2)
 
 s = Solution()
 print(s.climbStairs2(38))
