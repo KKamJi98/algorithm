6c1523f1cc0d5d17902f54fa1957934b24b4ee97
War-Oxi <xowl5460@naver.com>
2024-06-11 22:21:26 +0900

    you're doing great

---
+++ Data_Structure/min_heap.py
@@ -0,0 +1,8 @@
+import heapq
+# parent node = (i-1) // 2 
+# child node = [left]2 * i + 1, [right]2 * i + 2
+min_heap = [5, 3, 9, 4, 1, 2, 6]
+heapq.heapify(min_heap)
+print(min_heap) # [1, 3, 2, 4, 5, 9, 6] 
+print(heapq.heappop(min_heap)) # 1
+print(min_heap) # [2, 3, 6, 4, 5, 9]
