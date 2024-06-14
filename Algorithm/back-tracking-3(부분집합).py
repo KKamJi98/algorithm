e5b588045189bac682608a47e41f3c890644df69
War-Oxi <xowl5460@naver.com>
2024-06-14 14:28:58 +0900

    you're doing great

---
+++ Algorithm/back-tracking-3(부분집합).py
@@ -0,0 +1,20 @@
+# nums = [1,2,3,4]로 만들 수 있는 부분집합을 모두 반환하시오
+
+def solution(nums):
+    result = []
+    
+    def back_tracking(start, curr):
+        if len(curr) == k:
+            result.append(curr[:])
+            return
+        
+        for i in range(start, len(nums)):
+            curr.append(nums[i])
+            back_tracking(i+1, curr)
+            curr.pop()
+            
+    for k in range(len(nums) + 1):
+        back_tracking(start = 0, curr = [])
+    return result
+
+print(solution(nums = [1,2,3,4]))
---
+++ leetcode/Medium/945-minimum-increment-to-make-array-unique.py
@@ -0,0 +1,44 @@
+# https://leetcode.com/problems/minimum-increment-to-make-array-unique
+from typing import List
+
+
+class Solution:
+    # 시간 초과 O(n^2) 
+    def solution_ver1(self, nums: List[int]) -> int:
+        nums_dict = {}
+        new_nums = []
+        count = 0
+        for num in nums:
+            if num not in nums_dict:
+                nums_dict[num] = True
+            else:
+                new_nums.append(num)
+
+        while new_nums:
+            tmp_list = []
+            for i in range(0, len(new_nums)):
+                count += 1
+                new_nums[i] += 1
+                if new_nums[i] not in nums_dict:
+                    nums_dict[new_nums[i]] = True
+                    tmp_list.append(new_nums[i])
+            for i in tmp_list:
+                new_nums.remove(i)
+
+        return count
+    # greedy O(nlogn)
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        nums.sort()
+        count = 0
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i - 1]:
+                increment = nums[i - 1] - nums[i] + 1
+                nums[i] += increment
+                count += increment
+        print(nums)
+        return count
+
+
+s = Solution()
+result = s.minIncrementForUnique([3, 2, 1, 1, 2, 7])
+print(result)
