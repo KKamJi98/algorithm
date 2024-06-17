3b53d9dd5f4debaee4a284adbb376593412f3424
root <root@KKamJi.>
2024-06-17 21:30:32 +0900

    you're doing great

---
+++ programmers/python/lv1/142086.py
@@ -0,0 +1,23 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/142086 - [ 가장 가까운 글자 ]
+
+def solution(s):
+    # O(n)
+
+    # s와 똑같은 크기를 갖는 배열 answer 선언 (0 초기화)
+    answer = [0 for _ in range(len(s))]
+    # dict 선언
+    dict = {}
+    # 현재 인덱스에 존재하는 문자열이 있는지 확인 (반복)
+    #   없으면 answer[cur_idx]에 -1 저장 후 dict에 현재 인덱스 저장
+    #   있으면 answer[cur_idx] = cur_idx - dict[s[cur_idx]] 후 dict 갱신
+    for cur_idx in range(0, len(answer)):
+        if s[cur_idx] not in dict:
+            answer[cur_idx] = -1
+        else:
+            answer[cur_idx] = cur_idx - dict[s[cur_idx]]
+        dict[s[cur_idx]] = cur_idx
+    print(answer)
+    return answer
+
+
+solution("banana")
---
+++ programmers/python/lv1/147355.py
@@ -0,0 +1,16 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/147355 - [크기가 작은 부분 문자열]
+def solution(t, p):
+    def sub_string_checker(t_sub) -> bool:
+        if int(t_sub) <= int(p):
+            return True
+        return False
+    
+    result = []
+    for idx in range(0, len(t)-len(p)+1):
+        tmp = t[idx:idx+len(p)]
+        if sub_string_checker(tmp):
+            result.append(tmp)
+    
+    return len(result)
+
+print(solution("500220839878", "7")) # result = 2
