821d758028c15eb4c95d78eb9ba0fe7f5edb8090
KKamji <xowl5460@naver.com>
2024-06-18 20:18:19 +0900

    you're doing great

---
+++ programmers/python/lv2/12939.py
@@ -0,0 +1,16 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12939 - [ 최댓값과 최솟값 ]
+import math
+
+def solution(s):
+    min, max = math.inf, -math.inf
+    
+    arr = list(map(int, s.split(" ")))
+    print(arr)
+    for i in arr:
+        if i < min:
+            min = i
+        if i > max:
+            max = i
+    return f"{min} {max}"
+
+print(solution("-1 -2 -3 -4"))
