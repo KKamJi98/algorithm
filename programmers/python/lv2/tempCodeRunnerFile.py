d02d284e4805b601f9b6ce2585f96b46314d62a1
KKamji <xowl5460@naver.com>
2024-06-24 17:43:53 +0900

    you're doing great

--- programmers/python/lv2/tempCodeRunnerFile.py
+++ programmers/python/lv2/tempCodeRunnerFile.py
@@ -1,20 +1,26 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/12946 - [JadenCase 문자열 만들기]
+# https://school.programmers.co.kr/learn/courses/30/lessons/12946 - [하노이의 탑]
 
-def solution(s):
-    result = []
-    words = s.split(' ')
+def hanoi(n, start, end, via):
+    if n == 1:
+        # print(f"Move disk 1 from {start} to {end}")
+        return [[start, end]]
     
-    for word in words:
-        if len(word) == 0:
-            result.append('')
-        else:
-            if word[0].isdigit():
-                result.append(word)
-            else:
-                result.append(word[0].upper() + word[1:].lower())
-    # print(result)
-    return ' '.join(result)
+    moves = []
+    # Step 1: Move n-1 disks from start to via
+    moves += hanoi(n-1, start, via, end)
     
-print(solution("for the last week"))  # "For The Last Week"
-print(solution("hello  world"))  # "Hello  World"
-print(solution("3people unFollowed me"))  # "3people Unfollowed Me"
+    # Step 2: Move the nth disk from start to end
+    moves.append([start, end])
+    print(f"Move disk {n} from {start} to {end}")
+    
+    # Step 3: Move the n-1 disks from via to end
+    moves += hanoi(n-1, via, end, start)
+    
+    return moves
+
+def solution(n):
+    return hanoi(n, 1, 3, 2)
+
+n = 3  # 원판의 개수
+moves = solution(n)
+print(moves)
