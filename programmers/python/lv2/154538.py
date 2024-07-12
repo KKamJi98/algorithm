37c890db78314927ae2ca0360fecdaddd8729692
KKamji <xowl5460@naver.com>
2024-07-13 01:00:31 +0900

    you're doing great

--- programmers/python/lv2/154538.py
+++ programmers/python/lv2/154538.py
@@ -1,34 +1,29 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/154538 - [숫자 변환하기]
 from collections import deque
 
-
+# 
 def solution(x, y, n):
+    if x == y: # 예외처리
+        return 0
+    
     q = deque()
     visited = {}
     q.append(x)
     visited[x] = 0
     while q:
         current = q.popleft()
-        if current * 2 <= y:
-            if current * 2 in visited:
+        for next_value in [current * 2, current * 3, current + n]:
+            if next_value > y or next_value in visited:
                 continue
-            q.append(current * 2)
-            visited[current * 2] = visited[current] + 1
-        if current * 3 <= y:
-            if current * 3 in visited:
-                continue
-            q.append(current * 3)
-            visited[current * 3] = visited[current] + 1
-        if current + n <= y:
-            if current + n in visited:
-                continue
-            q.append(current + n)
-            visited[current + n] = visited[current] + 1
+            
+            q.append(next_value)
+            visited[next_value] = visited[current] + 1
+            
+            if next_value == y:
+                return visited[next_value]
 
-    if y in visited:
-        return visited[y]
-    else:
+    if y not in visited:
         return -1
 
 
-print(solution(10, 40, 2))  # 예상 출력: 3
+print(solution(5, 5, 4))  # 예상 출력: 3
