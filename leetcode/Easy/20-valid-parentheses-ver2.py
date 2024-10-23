08fa85cd4a923492add711d4de48cd0c76ec052a
KKamji <xowl5460@naver.com>
2024-10-23 16:06:56 +0900

    you're doing great

---
+++ leetcode/Easy/20-valid-parentheses-ver2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_map = {")": "(", "}": "{", "]": "["}
+        open_brackets = set(bracket_map.values())
+
+        for char in s:
+            if char in open_brackets:
+                stack.append(char)
+            elif char in bracket_map:
+                if not stack or stack[-1] != bracket_map[char]:
+                    return False
+                stack.pop()
+            else:
+                return False
+
+        return not stack
+
+
+solution_test = Solution()
+print(solution_test.isValid("(])"))  # 출력: False
+print(solution_test.isValid("()[]{}"))  # 출력: True
+print(solution_test.isValid("([)]"))  # 출력: False
+print(solution_test.isValid("{[]}"))  # 출력: True
