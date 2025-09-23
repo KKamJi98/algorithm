3c972db30ee147ca6aa6b96296852743de77f96e
KKamJi <xowl5460@naver.com>
2025-09-23 22:45:25 +0900

    feat: add Baekjoon 3273 two-pointer solution

---
+++ backjoon/python/3273_two_pointer.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/3273 - 두 수의 합 (Silver 3)
+# Two Pointer 를 사용해 O(n) 구현
+
+n = int(input())
+arr = sorted(list(map(int, input().split())))
+x = int(input())
+
+cnt = 0
+l, r = 0, n-1
+
+while l < r:
+    s = arr[l] + arr[r]
+    if s == x:
+        cnt += 1
+        l += 1
+        r -= 1
+    elif s < x:
+        l += 1
+    else:
+        r -= 1
+        
+print(cnt)
